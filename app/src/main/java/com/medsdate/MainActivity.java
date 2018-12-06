71888e9da34799f74a5daaabea70ee392a0bb91f
Gianni <gianni.cavalli@dshare.com>
2018-12-06 15:01:21 +0100

    wip credits added billing/donate feature added license copyright

--- app/src/main/java/com/medsdate/MainActivity.java
+++ app/src/main/java/com/medsdate/MainActivity.java
@@ -17,29 +17,44 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.android.billingclient.api.SkuDetails;
+import com.medsdate.billing.BillingHandler;
 import com.medsdate.data.db.model.MedicineEntry;
 import com.medsdate.notification.Receiver;
+import com.medsdate.ui.dialogs.DialogCreditsFragment;
 import com.medsdate.ui.dialogs.DialogMedicineFragment;
 import com.medsdate.ui.main.MedsAdapter;
 import com.medsdate.ui.viewmodel.MedsViewModel;
 import com.medsdate.utils.ItemClickSupport;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import timber.log.Timber;
 
 import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, DialogMedicineFragment.OnDialogMedicineListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DialogMedicineFragment.OnDialogMedicineListener,
+    BillingHandler.BillingCallbacks{
 
     private MedsViewModel mViewModel;
 
     private MedsAdapter mAdapter;
 
+    private BillingHandler mBilling;
+    private static final List<String> SKU = Arrays.asList("donation.regular", "donation.large");
+    private boolean mHideSnackbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        mBilling = new BillingHandler(this, this, SKU);
 
         findViewById(R.id.fab).setOnClickListener(this);
         mViewModel = ViewModelProviders.of(this).get(MedsViewModel.class);
@@ -162,9 +177,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.action_credits:
+                DialogCreditsFragment.newInstance()
+                        .show(getSupportFragmentManager(), "DialogCreditsFragment");
+                return true;
+            case R.id.action_donate:
+                showDonation();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        mBilling.destroy();
+        super.onDestroy();
+    }
+
+    private void showDonation(){
+        android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this)
+                .setTitle(R.string.donate_title)
+                .setMessage(R.string.donate_desc)
+                .setNeutralButton(android.R.string.cancel, null);
+
+        List<SkuDetails> skus = new ArrayList<>(getSkus());
+        Collections.sort(skus, new Comparator<SkuDetails>() {
+            @Override
+            public int compare(SkuDetails o1, SkuDetails o2) {
+                return Long.compare(o1.getPriceAmountMicros(), o2.getPriceAmountMicros());
+            }
+        });
+
+        for (int i = 0; i < skus.size() && i < 2; i++) {
+            final SkuDetails sku = skus.get(i);
+
+            String price = sku.getPrice();
+            DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    buy(sku);
+                }
+            };
+
+            if (i == 0) {
+                // Cheapest IAP
+                builder.setNegativeButton(price, listener);
+            } else {
+                // More expensive IAP
+                builder.setPositiveButton(price, listener);
+            }
+        }
+        builder.create().show();
+    }
+
+    private void buy(SkuDetails sku) {
+        mBilling.buy(sku);
+    }
+
+    private Collection<SkuDetails> getSkus() {
+        return mBilling.getSkus();
+    }
+
+    @Override
+    public void onStateChanged(boolean connected) {
+        Timber.w("onStateChanged " + connected);
+    }
+
+    @Override
+    public void onPurchased(SkuDetails sku, boolean isNew) {
+        Timber.w("onPurchased " + sku.getSku() + " " + isNew);
+        if (!isNew) {
+            if (mHideSnackbar) {
+                return;
+            } else {
+                mHideSnackbar = true;
+            }
+        }
+        Snackbar.make(findViewById(android.R.id.content),
+                isNew ? R.string.donate_new : R.string.donate_history,
+                Snackbar.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/medsdate/ui/dialogs/DialogCreditsFragment.java
@@ -0,0 +1,71 @@
+package com.medsdate.ui.dialogs;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.medsdate.R;
+
+public class DialogCreditsFragment extends DialogFragment {
+
+    private boolean cancelable;
+
+    private OnDialogCreditsListener listener;
+
+    private View dialogView;
+
+    public static DialogCreditsFragment newInstance() {
+        return newInstance(true, null);
+    }
+
+    public static DialogCreditsFragment newInstance(boolean cancelable, OnDialogCreditsListener listener) {
+        DialogCreditsFragment fragment = new DialogCreditsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+
+        fragment.cancelable = cancelable;
+
+        if(listener != null) {
+            fragment.listener = listener;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        this.setCancelable(cancelable);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        dialogView = inflater.inflate(R.layout.dialog_credits, null);
+
+        // Create the AlertDialog object and return it
+        builder.setView(dialogView);
+        Dialog dialog = builder.create();
+
+        TextView title = dialogView.findViewById(R.id.title_credits_text);
+        title.setText(R.string.title_credits_text);
+        TextView message = dialogView.findViewById(R.id.msg_credits_text);
+        message.setText(getString(R.string.msg_credits_text));
+
+        dialog.setCanceledOnTouchOutside(cancelable);
+
+        return builder.create();
+    }
+
+    public interface OnDialogCreditsListener {
+    }
+}
