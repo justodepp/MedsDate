ddc55d605d2b1b15a6a39c8a0f59eeb97b103fd1
Gianni <gianni.cavalli@dshare.com>
2018-12-06 15:48:26 +0100

    fix Firebase Crashlytics fix Localization Language

--- app/src/main/java/com/medsdate/BasicApp.java
+++ app/src/main/java/com/medsdate/BasicApp.java
@@ -6,11 +6,14 @@ package com.medsdate;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.facebook.stetho.Stetho;
 import com.medsdate.data.db.AppDatabase;
 import com.medsdate.data.db.MedsRepository;
 import com.medsdate.utils.AppExecutors;
 
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 public class BasicApp extends Application {
@@ -21,6 +24,10 @@ public class BasicApp extends Application {
     public void onCreate() {
         super.onCreate();
 
+        CrashlyticsCore core = new CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG)
+                .build();
+        Fabric.with(this, new Crashlytics.Builder().core(core).build());
         Stetho.initializeWithDefaults(this);
         Timber.plant(new Timber.DebugTree());
         mAppExecutors = AppExecutors.getInstance();
