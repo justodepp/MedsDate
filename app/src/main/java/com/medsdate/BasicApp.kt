22c4b3befe822e8ad7d2da0399d35ee8bf12eabf
Gianni <gianni.cavalli@dshare.com>
2019-01-04 17:54:54 +0100

    removed unused code from Application class

--- app/src/main/java/com/medsdate/BasicApp.kt
+++ app/src/main/java/com/medsdate/BasicApp.kt
@@ -5,26 +5,16 @@ package com.medsdate
  */
 
 import android.app.Application
-
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
 import com.facebook.stetho.Stetho
-import com.medsdate.data.db.AppDatabase
-import com.medsdate.data.db.MedsRepository
 import com.medsdate.utils.AppExecutors
-
 import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class BasicApp : Application() {
 
     private var mAppExecutors: AppExecutors? = null
-
-    val database: AppDatabase
-        get() = AppDatabase.getInstance(this, mAppExecutors)
-
-    val repository: MedsRepository
-        get() = MedsRepository.getInstance(database)
 
     override fun onCreate() {
         super.onCreate()
