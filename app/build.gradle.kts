103a3fd85c2c7c584e131c1f8eac45df090fa2b0
gianni <gianni@ovolab.com>
2025-10-16 12:42:14 +0200

    Updated app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.medsdate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
